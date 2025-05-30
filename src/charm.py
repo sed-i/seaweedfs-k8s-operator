9763bb6d236f1b421365e9c140f14a94033cefef
sed-i <82407168+sed-i@users.noreply.github.com>
2025-05-30 14:09:11 -0400

    Init commit

---
+++ src/charm.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# Copyright 2025 him
+# See LICENSE file for licensing details.
+
+"""Charm the application."""
+
+import logging
+
+import ops
+
+logger = logging.getLogger(__name__)
+
+
+class SeaweedfsK8SOperatorCharm(ops.CharmBase):
+    """Charm the application."""
+
+    def __init__(self, framework: ops.Framework):
+        super().__init__(framework)
+        framework.observe(self.on["some_container"].pebble_ready, self._on_pebble_ready)
+
+    def _on_pebble_ready(self, event: ops.PebbleReadyEvent):
+        """Handle pebble-ready event."""
+        self.unit.status = ops.ActiveStatus()
+
+
+if __name__ == "__main__":  # pragma: nocover
+    ops.main(SeaweedfsK8SOperatorCharm)
---
+++ tests/unit/test_charm.py
@@ -0,0 +1,21 @@
+# Copyright 2025 him
+# See LICENSE file for licensing details.
+#
+# Learn more about testing at: https://juju.is/docs/sdk/testing
+
+from ops import testing
+
+from charm import SeaweedfsK8SOperatorCharm
+
+
+def test_pebble_ready():
+    # Arrange:
+    ctx = testing.Context(SeaweedfsK8SOperatorCharm)
+    container = testing.Container("some-container", can_connect=True)
+    state_in = testing.State(containers={container})
+
+    # Act:
+    state_out = ctx.run(ctx.on.pebble_ready(container), state_in)
+
+    # Assert:
+    assert state_out.unit_status == testing.ActiveStatus()
