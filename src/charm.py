1a2b4ba2d774bd1887e501ee41135bff220de913
sed-i <82407168+sed-i@users.noreply.github.com>
2025-05-30 15:28:18 -0400

    Fix

--- src/charm.py
+++ src/charm.py
@@ -14,14 +14,51 @@ logger = logging.getLogger(__name__)
 class SeaweedfsK8S(ops.CharmBase):
     """Charm the application."""
 
+    container_name = "seaweedfs"
+    _storage_path = "/data"
+
     def __init__(self, framework: ops.Framework):
         super().__init__(framework)
-        framework.observe(self.on["seaweedfs"].pebble_ready, self._on_pebble_ready)
-
-    def _on_pebble_ready(self, event: ops.PebbleReadyEvent):
-        """Handle pebble-ready event."""
+        self.reconcile()
+        
+    def reconcile(self):
+        container = self.unit.get_container(self.container_name)
+        container.add_layer(
+            self.container_name, self._pebble_layer(""), combine=True
+        )
         self.unit.status = ops.ActiveStatus()
+    
+    def _pebble_layer(self, sentinel: str) -> Layer:
+        """Construct the Pebble layer information.
+
+        Args:
+            sentinel: A value indicative of a change that should prompt a replan.
+        """
+        layer = Layer(
+            {
+                "summary": "seaweedfs-k8s layer",
+                "description": "seaweedfs-k8s layer",
+                "services": {
+                    self.container_name: {
+                        "override": "replace",
+                        "summary": "seaweedfs-k8s service",
+                        "command": f"/usr/bin/weed server -dir={self._storage_path} -s3",
+                        "startup": "enabled",
+                        "environment": {
+                            "_config_hash": sentinel,  # Restarts the service via pebble replan
+                            "https_proxy": os.environ.get("JUJU_CHARM_HTTPS_PROXY", ""),
+                            "http_proxy": os.environ.get("JUJU_CHARM_HTTP_PROXY", ""),
+                            "no_proxy": os.environ.get("JUJU_CHARM_NO_PROXY", ""),
+                        },
+                    }
+                },
+            }
+        )
+
+        return layer
+    
+
 
 
 if __name__ == "__main__":  # pragma: nocover
-    ops.main(SeaweedfsK8SOperatorCharm)
+    ops.main(SeaweedfsK8S)
